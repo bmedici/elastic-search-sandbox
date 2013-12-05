0433a462c7f99aa4374dd07cf116dd93c949a1de
Bruno <bmgit@bmconseil.com>
2013-12-06 00:53:19 +0100

    moved constants outside

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 require 'elasticsearch'
-ES_SOURCE = '_source'
-ES_ID = '_id'
-ES_TYPE = '_type'
-ES_SCORE = '_score'
-ES_STAMP = '_stamp'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+ES_SOURCE = '_source'
+ES_ID = '_id'
+ES_TYPE = '_type'
+ES_SCORE = '_score'
+
+ES_STAMP = '!stamp'
+ES_ATTR = '!attr'
+ES_DEBUG = '!debug'
