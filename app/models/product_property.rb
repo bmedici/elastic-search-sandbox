bbdc40d7192079eda7e670331c1201f006bb566b
Bruno <bmgit@bmconseil.com>
2013-12-08 15:07:34 +0100

    updated ProductProperty migration

--- app/models/product_property.rb
+++ app/models/product_property.rb
@@ -1,5 +1,4 @@
 class ProductProperty < ActiveRecord::Base
-  # attr_accessible :title, :body
   belongs_to :product
   belongs_to :property
   belongs_to :value
--- db/migrate/20131207174326_create_product_properties.rb
+++ db/migrate/20131207174326_create_product_properties.rb
@@ -1,10 +1,10 @@
 class CreateProductProperties < ActiveRecord::Migration
   def change
   	create_table :product_properties, :id => false do |t|
-        t.references :product
-        t.references :property
-        t.references :value
-        t.string :other
+        t.references :product, nil: false
+        t.references :property, nil: false
+        t.references :value, default: nil
+        t.string :other, default: nil
     end
   	add_index :product_properties, :product_id
   	add_index :product_properties, :property_id
